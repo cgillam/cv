a6a9a6afb2cb774d1c137f15eff3b2a7f41c5768
cgillam <62260365+cgillam@users.noreply.github.com>
2020-08-28 13:54:27 -0600

    added projects

--- src/projects.js
+++ src/projects.js
@@ -5,8 +5,60 @@ export default [
         description: "My portfolio",
         deployed: "https://google.com",
         github: "https://github.com",
-        images: [
+        images: [ // TODO
             "https://placekitten.com/200/300"
+        ]
+    },
+    {
+        slug: 'budget-tracker',
+        title: "Budget Tracker",
+        description: "A offline budget tracker",
+        deployed: "https://mighty-refuge-37354.herokuapp.com/",
+        github: "https://github.com/cgillam/bank",
+        images: [
+            "https://i.imgur.com/pR945cz.png"
+        ]
+    },
+    {
+        slug: 'fetch',
+        title: "Fetch",
+        description: "Tinder for dogs",
+        deployed: "https://voyage-choucroute-01318.herokuapp.com/",
+        github: "https://github.com/PaulFidika/Secret-Project-2",
+        images: [
+            "https://i.imgur.com/c8emRK4.jpg",
+            "https://i.imgur.com/sJ2ASup.jpg",
+            "https://i.imgur.com/9lgLoPX.jpg",
+            "https://i.imgur.com/4QddqpF.jpg"
+        ]
+    },
+    {
+        slug: 'mini-mixologist',
+        title: "Mini Mixologist",
+        description: "A cocktail making app",
+        deployed: "https://cgillam.github.io/mini-mixologist/",
+        github: "https://github.com/cgillam/mini-mixologist",
+        images: [
+            "https://i.imgur.com/BZ9V44v.png",
+            "https://i.imgur.com/AhwxK94.png"
+        ]
+    },
+    {
+        slug: '5x5-tracker',
+        title: "5x5 tracker",
+        description: "Workout training tracker",
+        deployed: "https://polar-dusk-87986.herokuapp.com/",
+        github: "https://github.com/cgillam/5x5",
+        images: [] //TODO
+    },
+    {
+        slug: 'burger-eater',
+        title: "Burger Eater",
+        description: "Eat burgers",
+        deployed: "https://boiling-retreat-71095.herokuapp.com",
+        github: "https://github.com/cgillam/burger",
+        images: [
+            "https://i.imgur.com/4McelLG.png"
         ]
     }
 ]
